6f415246c260971423538c891f294c04d19da12f
casaburo.rocco <casaburo.rocco@a0ca4d19-ec3b-1be5-d93c-5ec95786afce>
2010-03-20 06:55:59 +0000

    Uploading of release 1.0.6 after repository reset

---
+++ trunk/src/org/casaburo/utils/textPopupMenu/Installer.java
@@ -0,0 +1,38 @@
+// <editor-fold defaultstate="collapsed" desc="license">
+/*
+ *  Copyright 2010 Rocco Casaburo.
+ *  mail address: rcp.nbm.casaburo at gmail.com
+ *  Visit projects homepage at http://sites.google.com/site/nbmprojects
+ *
+ *  Licensed under the GNU General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       http://www.gnu.org/licenses/gpl-3.0.html
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+// </editor-fold>
+package org.casaburo.utils.textPopupMenu;
+
+import java.awt.Toolkit;
+import org.openide.modules.ModuleInstall;
+import org.openide.windows.WindowManager;
+
+public class Installer extends ModuleInstall {
+
+   @Override
+    public void restored() {
+       //modfica
+        WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
+            @Override
+            public void run() {
+                PopupMenuEventQueue evento = new PopupMenuEventQueue();
+                Toolkit.getDefaultToolkit().getSystemEventQueue().push(evento);
+           }
+        });
+    }
+}
