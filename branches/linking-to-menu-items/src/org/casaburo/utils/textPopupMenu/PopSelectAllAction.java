bed03a00ad05f085eb9f6ad1b911ea9a8542f630
casaburo.rocco <casaburo.rocco@a0ca4d19-ec3b-1be5-d93c-5ec95786afce>
2010-04-22 15:26:53 +0000

    Copy to a new branch in order to implement a new feature: link actions to Edit menu items

---
+++ branches/linking-to-menu-items/src/org/casaburo/utils/textPopupMenu/PopSelectAllAction.java
@@ -0,0 +1,43 @@
+// <editor-fold defaultstate="collapsed" desc="license">
+/*
+ *  Copyright 2010 Rocco Casaburo.
+ *  mail address: rcp.nbm.casaburo at gmail.com
+ *  Visit projects homepage at http://sites.google.com/site/nbmprojects
+ *
+ *  Licensed under the GNU General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       http://www.gnu.org/licenses/gpl-3.0.html
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+// </editor-fold>
+package org.casaburo.utils.textPopupMenu;
+
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.KeyStroke;
+
+ class PopSelectAllAction extends BasicAction {
+
+    public PopSelectAllAction(String text, Icon icon) {
+        super(text, icon);
+        putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl A"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        comp.selectAll();
+       
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return comp != null && comp.isEnabled() && comp.getText().length() > 0 && (comp.getSelectedText() == null || comp.getSelectedText().length() < comp.getText().length());
+    }
+}
---
+++ branches/linking-to-menu-items/src/org/casaburo/utils/textPopupMenu/PopupMenuEventQueue.java
@@ -0,0 +1,135 @@
+// <editor-fold defaultstate="collapsed" desc="license">
+/*
+ *  Copyright 2010 Rocco Casaburo.
+ *  mail address: rcp.nbm.casaburo at gmail.com
+ *  Visit projects homepage at http://sites.google.com/site/nbmprojects
+ *
+ *  Licensed under the GNU General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       http://www.gnu.org/licenses/gpl-3.0.html
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+// </editor-fold>
+package org.casaburo.utils.textPopupMenu;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ResourceBundle;
+import javax.swing.*;
+import javax.swing.text.*;
+import org.openide.util.ImageUtilities;
+import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
+
+/**
+ * how to use: it is enabled adding the following code row into your own code:
+ * Toolkit.getDefaultToolkit().getSystemEventQueue().push( new PopupMenuEventQueue());
+ *<p>
+ *Utilizzo: viene abilitato in automatico aggiungendo la seguente riga di codice:
+ *Toolkit.getDefaultToolkit().getSystemEventQueue().push(new PopupMenuEventQueue());
+ *
+ */
+class PopupMenuEventQueue extends EventQueue {
+
+    public JPopupMenu popup;
+    public BasicAction cut, copy, paste, selectAll;
+    private final String cutString;
+    private final String CUT_ICON_PATH;
+    private final ImageIcon cutIcon;
+    private final String copyString;
+    private final String COPY_ICON_PATH;
+    private final ImageIcon copyIcon;
+    private final String pasteString;
+    private final String PASTE_ICON_PATH;
+    private final ImageIcon pasteIcon;
+    private final ResourceBundle bundle;
+    private ActionMap actionMap;
+
+    PopupMenuEventQueue() {
+        cutString = NbBundle.getMessage(org.openide.actions.CutAction.class, "Cut").replace("&", "");
+        CUT_ICON_PATH = "org/openide/resources/actions/cut.gif";
+        cutIcon = (ImageUtilities.loadImageIcon(CUT_ICON_PATH, true));
+
+        copyString = NbBundle.getMessage(org.openide.actions.CopyAction.class, "Copy").replace("&", "");
+        COPY_ICON_PATH = "org/openide/resources/actions/copy.gif";
+        copyIcon = (ImageUtilities.loadImageIcon(COPY_ICON_PATH, true));
+
+        pasteString = NbBundle.getMessage(org.openide.actions.PasteAction.class, "Paste").replace("&", "");
+        PASTE_ICON_PATH = "org/openide/resources/actions/paste.gif";
+        pasteIcon = (ImageUtilities.loadImageIcon(PASTE_ICON_PATH, true));
+
+        bundle = ResourceBundle.getBundle("org/casaburo/utils/textPopupMenu/resources/PopUpBundle");
+
+    }
+
+    public void createPopupMenu(JTextComponent textComponent) {
+
+        //TODO: completare con l'internazionalizzazione del comando Select All
+
+        cut = new PopCutAction(cutString, cutIcon);//(messages.getString("CutString"),null);
+        copy = new PopCopyAction(copyString, copyIcon);//(messages.getString("CopyString"), null);
+        paste = new PopPasteAction(pasteString, pasteIcon);//(messages.getString("PasteString"),null);
+        selectAll = new PopSelectAllAction(bundle.getString("SelectAllString"), null);
+        cut.setTextComponent(textComponent);
+        copy.setTextComponent(textComponent);
+        paste.setTextComponent(textComponent);
+        selectAll.setTextComponent(textComponent);
+
+        popup = new JPopupMenu();
+        popup.add(cut);
+        popup.add(copy);
+        popup.add(paste);
+        popup.addSeparator();
+        popup.add(selectAll);
+    }
+
+    public void showPopup(Component parent, MouseEvent mouseEvt) {
+        popup.validate();
+        popup.show(parent, mouseEvt.getX(), mouseEvt.getY());
+    }
+
+    //TODO: rendere indipendente dalla piattaforma le shortcuts dei comandi
+    @Override
+    protected void dispatchEvent(AWTEvent event) {
+        super.dispatchEvent(event);
+
+        if (!(event instanceof MouseEvent)) {
+            return;
+        }
+        MouseEvent me = (MouseEvent) event;
+        Component component = SwingUtilities.getDeepestComponentAt((Component) me.getSource(), me.getX(), me.getY());
+       actionMap=retrieveTopComponentOf(component).getActionMap();
+
+
+        if (!me.isPopupTrigger()) {
+            return;
+        }
+        if (!(me.getSource() instanceof Component)) {
+            return;
+        }
+        if (!(component instanceof JTextComponent)) {
+            return;
+        }
+        if (MenuSelectionManager.defaultManager().getSelectedPath().length > 0) {
+            return;
+        }
+        component.requestFocus(); //Thanks to wsc719@yahoo.com.cn for this contribution
+        createPopupMenu((JTextComponent) component);
+        showPopup((Component) me.getSource(), me);
+    }
+
+    private TopComponent retrieveTopComponentOf(Component c) {
+        TopComponent tc = null;
+        tc = (TopComponent) SwingUtilities.getAncestorOfClass(org.openide.windows.TopComponent.class, c);
+
+
+        return tc;
+        //     TODO get TopComponent tc related ActionMap and link actions to the Edit menu
+          }
+}
